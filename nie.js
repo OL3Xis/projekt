8b9c8339f4865ca115db3fa44cd5619ecf014a29
OL3Xis <105917672+OL3Xis@users.noreply.github.com>
2022-05-20 11:50:16 +0200

    Add files via upload

---
+++ nie.js
@@ -0,0 +1,9 @@
+function zmiana() {
+    document.getElementById("jeremy").style.backgroundImage = "url('https://i.ytimg.com/vi/IrZtV_0SAz4/maxresdefault.jpg')";
+    console.log("eee")
+}
+
+function zmiana2() {
+    document.getElementById("jeremy").style.backgroundImage = "url('https://wrc.net.pl/app/uploads/2020/10/jeremy-clarkson-skr%C4%99t-top-gear.png')";
+    console.log("eee")
+}
